bb60d9a97a478b484b770dc70d1107bdbe4ed051
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-07 13:54:16 -0400

    Agregar rutas para todos los handlebars

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -5,4 +5,24 @@ publicRoutes.get('/', (req, res) => {
     res.render('index');
 });
 
+publicRoutes.get('/registro', (req, res) => {
+    res.render('registro');
+});
+
+publicRoutes.get('/login', (req, res) => {
+    res.render('login');
+});
+
+publicRoutes.get('/datos', (req, res) => {
+    res.render('datos');
+});
+
+publicRoutes.get('/admin', (req, res) => {
+    res.render('admin');
+});
+
+
+
+
+
 module.exports = publicRoutes;
