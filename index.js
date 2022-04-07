2d38fdb834c013b00f6f4c7f113e410759d3fac0
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-07 13:51:55 -0400

    Configurar handlebars para servir páginas

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 const publicRoutes = require('./routes/public.routes');
 const app = express();
 const port = 3000;
 
 // Configuración express
 app.use(express.json());
+app.use(express.static('./public')); //  define ruta para archivos estáticos
 
+// Configuración handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Rutas
 app.use('/', publicRoutes); // rutas públicas (para el html)
 
 app.listen(port, () => {
--- routes/public.routes.js
+++ routes/public.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const publicRoutes = express.Router();
 
 publicRoutes.get('/', (req, res) => {
-
+    res.render('index');
 });
 
 module.exports = publicRoutes;
