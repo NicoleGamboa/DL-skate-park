30250fee51f2f76dbb97e4e7fb40d41e8a6de23e
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-09 11:26:27 -0400

    Actualizar variables de base de datos

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 const config = require('./config');
 
 const connectionData = {
-    user: config.database.user || 'postgres',
-    host: config.database.host || 'localhost',
-    database: config.database.url || 'skatepark',
-    password: config.database.password || 'carvajal0995',
-    port: config.database.port || 5432,
+    user: config.database.user,
+    host: config.database.host,
+    database: config.database.name,
+    password: config.database.password,
+    port: config.database.port,
 }
 
 const client = new Pool(connectionData)
