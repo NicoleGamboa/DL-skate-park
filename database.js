482ffc566b4a0d2c9a5c77e16b0aab93b3201ed1
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-09 10:32:55 -0400

    Agragar configuración de base de datos para heroku

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 const connectionData = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'skatepark',
-    password: 'carvajal0995',
-    port: 5432,
+    user: process.env.DATABASE_USER || 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    database: process.env.DATABASE || 'skatepark',
+    password: process.env.DATABASE_PASSWORD || 'carvajal0995',
+    port: process.env.DATABASE_PORT || 5432,
 }
 
 const client = new Pool(connectionData)
