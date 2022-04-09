77663a37d4b2464d52e674711cb1932f73d57ed0
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-09 11:40:45 -0400

    Eliminar indentación innecesaria

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     database: {
         user: process.env.DATABASE_USER || '',
         host: process.env.DATABASE_HOST || '',
-        url: process.env.DATABASE || '',
+        name: process.env.DATABASE || '',
         password: process.env.DATABASE_PASSWORD || '',
         port: process.env.DATABASE_PORT || 5432,
     },
