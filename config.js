b7e222e90c78b1ef8b90a094e750ab7a505fafcc
Nicole Gamboa <nicole.gamboacifuentes@gmail.com>
2022-04-09 11:54:06 -0400

    Agregar textos para prueba heroku

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     database: {
-        user: process.env.DATABASE_USER || '',
-        host: process.env.DATABASE_HOST || '',
-        name: process.env.DATABASE || '',
-        password: process.env.DATABASE_PASSWORD || '',
+        user: process.env.DATABASE_USER || 'nvkrwzcuuefgko',
+        host: process.env.DATABASE_HOST || 'ec2-18-214-134-226.compute-1.amazonaws.com',
+        name: process.env.DATABASE || 'ddalo6a9l5thrn',
+        password: process.env.DATABASE_PASSWORD || '3850425fd064eaed6e559cc68ef1b9d6afcf684e6f5ff5f0b9424d91f6fcfffe',
         port: process.env.DATABASE_PORT || 5432,
     },
     app: {
